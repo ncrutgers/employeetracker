ed15a1f332895cb17156dd5775e814128ffe3220
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-05-28 02:03:09 -0400

    Added runSearch function; inquirer prompts of choice actions

--- employeeTracker.js
+++ employeeTracker.js
@@ -13,4 +13,56 @@ connection.connect(function(err) {
     if (err) throw err;
 
     console.log("Connected as id " + connection.threadId);
+    runSearch();
 });
+
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a department",
+        "Add a role",
+        "Add an employee", 
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update an employee's role"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add a department":
+        addDepartment();
+        break;
+
+      case "Add a role":
+        addRole();
+        break;
+
+      case "Add an employee":
+        addEmployee();
+        break;
+
+      case "View departments":
+        viewDepartments();
+        break;
+
+      case "View roles":
+        viewRoles();
+        break;
+
+      case "View employees":
+        viewEmployees();
+        break;
+        
+      case "Update an employee's role":
+        updateEmployeeRole();
+        break;  
+      }
+    });
+}
+
